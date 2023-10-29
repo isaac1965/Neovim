4945c22a5e965d1df1708701c1b966b5326612a3
Isaac Maldonado <isaacmaldonado21@gmail.com>
2023-10-29 14:15:59 -0400

    Update lua.init

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
+
+--██ ███    ██ ██ ████████    ██      ██    ██  █████  
+--██ ████   ██ ██    ██       ██      ██    ██ ██   ██ 
+--██ ██ ██  ██ ██    ██       ██      ██    ██ ███████ 
+--██ ██  ██ ██ ██    ██       ██      ██    ██ ██   ██ 
+--██ ██   ████ ██    ██    ██ ███████  ██████  ██   ██ 
+                                                     
+                                                     
+
 require('settings') -- settings
 require('keymaps') -- keymaps
 require('plugins/packer') -- plugin manager
-require('plugins/tokyonight') -- color theme
+require('plugins/theme') -- color theme
 require('plugins/surround') -- plugin surround
 require('plugins/lualine') -- plugin lualine
 require('plugins/nvim-tree') -- plugin nvm-tree
@@ -10,3 +19,4 @@ require('plugins/devicons') -- plugins devicons
 require('plugins/coc') -- plugin coc.nvim
 require('plugins/emmet') -- plugin emmet
 require('plugins/indentline') -- plugin emmet
+--require('plugins/treesitter') -- plugin emmet
