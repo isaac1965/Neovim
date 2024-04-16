8080ec7740cdf71b63dc9c96ee6c752fb7febca1
Isaac Maldonado <isaacmaldonado21@gmail.com>
2024-04-16 10:57:22 -0400

    Tue Apr 16 10:56:48 AM -04 2024

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,30 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+return {
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		lazy = true,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VeryLazy",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+        --style = "onedark"
+        pickers = {
+          find_files = {
+            cwd = "/home/isaacbspwm",
+            find_command = { "find", ".", "-type", "f", "-printf", "%P\n" },
+            hidden = true,
+          },
+        },
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+}
