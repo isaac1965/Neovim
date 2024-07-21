8d6f0989b9878a8abeb6b83add55c41b1e070564
Isaac <44930181+isaac1965@users.noreply.github.com>
2024-07-21 08:59:17 -0400

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,33 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		lazy = true,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		event = "VeryLazy",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-        --style = "onedark"
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    lazy = true,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          -- otros valores predeterminados
+        },
+        -- finding hidden files
         pickers = {
           find_files = {
-            cwd = "/home/isaacbspwm",
-            find_command = { "find", ".", "-type", "f", "-printf", "%P\n" },
-            hidden = true,
+            find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" }
+          }
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
           },
         },
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+        --style = "onedark"
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
+
